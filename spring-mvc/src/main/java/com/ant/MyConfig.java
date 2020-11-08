5c78d474396c6aebae0ef420fc680078aa04cb3a
ant <xuchuanliang361@163.com>
2020-11-08 22:09:20 +0800

    Servlet 3.0规范

---
+++ spring-mvc/src/main/java/com/ant/MyConfig.java
@@ -0,0 +1,9 @@
+package com.ant;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ant")
+public class MyConfig {
+}
---
+++ spring-mvc/src/main/java/com/ant/MyTestImpl.java
@@ -0,0 +1,4 @@
+package com.ant;
+
+public class MyTestImpl implements MyTest{
+}
